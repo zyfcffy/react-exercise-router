4a093e7e56809374f907bf41a44c7d69d9ebbb10
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-23 23:13:52 +0800

    [fufangyuan]complete 4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router,Switch} from 'react-router-dom';
 import {Route} from "react-router";
 import Home from "./Home";
-import About from "./About";
 import User from "./User";
+import NotMatch from "./NotMatch";
 
 class App extends Component {
   render() {
@@ -12,8 +12,9 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path='/' component={Home} /> 
-            <Route exact path='/about' component={About} />
+            <Route exact path='/about' component={User} />
             <Route exact path='/:user' component={User} />
+            <Route exact component={NotMatch} />
           </Switch>
         </Router>
       </div>
